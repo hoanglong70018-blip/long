6928d43b8917630e7de06b80193c6921f517b0b9
longbest <hoangphuonglong07@gmail.com>
2026-01-07 16:29:27 +0700

    chore: enable dynamic base path for vercel deployment

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import CalendarPage from './pages/CalendarPage';
+import BlogPage from './pages/BlogPage';
 import { CalendarProvider } from './context/CalendarContext';
+import { PopupProvider } from './context/PopupContext';
 
 function App() {
   return (
-    <CalendarProvider>
-      <Router basename="/long">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/calendar" element={<CalendarPage />} />
-        </Routes>
-      </Router>
-    </CalendarProvider>
+    <PopupProvider>
+      <CalendarProvider>
+        <Router basename={import.meta.env.BASE_URL}>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/calendar" element={<CalendarPage />} />
+            <Route path="/blog" element={<BlogPage />} />
+          </Routes>
+        </Router>
+      </CalendarProvider>
+    </PopupProvider>
   );
 }
 
